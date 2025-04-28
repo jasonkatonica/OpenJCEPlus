bb09ea1cd609c1c73da0513ea3164c42385790bf
Jason Katonica <katonica@us.ibm.com>
2025-04-28 11:10:56 -0400

    more

--- src/test/java/ibm/jceplus/jmeter/SecretKeyFactoryBenchmark.java
+++ src/test/java/ibm/jceplus/jmeter/SecretKeyFactoryBenchmark.java
@@ -28,29 +28,24 @@ public class SecretKeyFactoryBenchmark extends AbstractJavaSamplerClient {
     private SecretKeyFactory secretKeyFactory;
     private char[] password;
     private byte[] salt;
-    private byte[] rawKeyData;
-    private int iterations;
-    private int keyLength;
+    private int iterations = 300000;
+    private int keyLength = 512;
     private String algorithm;
     private String provider;
-    private String keyType;
     private int threads;
     private int loops;
     private Random random = new Random();
     private String osName = null;
     private String osArch = null;
     private String javaVersion = null;
-
+    private static final String PASSWORD = "thisisareasonablesizedpassword";
 
     @Override
     public void setupTest(JavaSamplerContext context) {
-        algorithm = context.getParameter("algorithm", "PBKDF2WithHmacSHA256");
-        iterations = Integer.parseInt(context.getParameter("iterations", "10000"));
-        keyLength = Integer.parseInt(context.getParameter("keyLength", "256"));
-        provider = context.getParameter("provider", "");
-        keyType = context.getParameter("keyType", "PBE").toUpperCase();
-        loops = Integer.parseInt(context.getParameter("loops", "5"));
-        threads = Integer.parseInt(context.getParameter("threads", "1"));
+        algorithm = context.getParameter("algorithm");
+        provider = context.getParameter("provider");
+        loops = Integer.parseInt(context.getParameter("loops"));
+        threads = Integer.parseInt(context.getParameter("threads"));
         osArch = System.getProperty("os.arch");
         osName = System.getProperty("os.name");
         javaVersion = System.getProperty("java.runtime.version");
@@ -58,35 +53,12 @@ public class SecretKeyFactoryBenchmark extends AbstractJavaSamplerClient {
         try {
             setupProvider();
 
-            if (provider != null && !provider.isEmpty()) {
-                secretKeyFactory = SecretKeyFactory.getInstance(algorithm, provider);
-            } else {
-                secretKeyFactory = SecretKeyFactory.getInstance(algorithm);
-            }
-
-            // Generate random password
-            String passwordStr = generateRandomString(12);
-            password = passwordStr.toCharArray();
-
-            // Generate random salt
+            secretKeyFactory = SecretKeyFactory.getInstance(algorithm, provider);
             salt = new byte[16];
             random.nextBytes(salt);
-
-            // Generate raw key data for non-PBE algorithms
-            rawKeyData = new byte[keyLength / 8];
-            random.nextBytes(rawKeyData);
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private String generateRandomString(int length) {
-        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
-        StringBuilder sb = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            sb.append(characters.charAt(random.nextInt(characters.length())));
-        }
-        return sb.toString();
     }
 
     private void setupProvider() throws Exception {
@@ -112,45 +84,28 @@ public class SecretKeyFactoryBenchmark extends AbstractJavaSamplerClient {
     @Override
     public SampleResult runTest(JavaSamplerContext context) {
         SampleResult result = new SampleResult();
-        result.setSampleLabel("SecretKeyFactoryBenchmark" + " Algorithm:" + algorithm + ", KeyType:"
-                + keyType + ", Iterations:" + iterations + ", KeyLength:" + keyLength
+        result.setSampleLabel("SecretKeyFactoryBenchmark" + " Algorithm:" + algorithm
+                + ", Iterations:" + iterations + ", KeyLength:" + keyLength
                 + ", Provider:" + provider + ", Threads:" + threads);
 
         try {
             SecretKey secretKey = null;
-            KeySpec spec = null;
+            KeySpec spec = new PBEKeySpec(PASSWORD.toCharArray(), salt, iterations, keyLength);
+
             result.sampleStart();
-
             for (int x = 0; x < loops; x++) {
-                switch (keyType) {
-                    case "PBE":
-                    case "PBKDF2":
-                        // For PBE/PBKDF2 algorithms
-                        spec = new PBEKeySpec(password, salt, iterations, keyLength);
-                        break;
-                    case "RAW":
-                        // For raw key conversion algorithms like DES, AES, etc.
-                        spec = new SecretKeySpec(rawKeyData, algorithm.split("/")[0]);
-                        break;
-                    default:
-                        // Default to PBE
-                        spec = new PBEKeySpec(password, salt, iterations, keyLength);
-                }
-
                 secretKey = secretKeyFactory.generateSecret(spec);
             }
-
             result.sampleEnd();
 
             // Using key length as bytes measurement
-            result.setBytes((long) loops * keyLength / 8);
+            result.setBytes((long) loops * keyLength);
             result.setResponseCodeOK();
             result.setResponseMessage("OK");
             result.setSuccessful(true);
 
             JMeterContextService.getContext().getVariables().put("provider", provider);
             JMeterContextService.getContext().getVariables().put("algorithm", algorithm);
-            JMeterContextService.getContext().getVariables().put("keyType", keyType);
             JMeterContextService.getContext().getVariables().put("iterations", Integer.valueOf(iterations).toString());
             JMeterContextService.getContext().getVariables().put("keyLength", Integer.valueOf(keyLength).toString());
             JMeterContextService.getContext().getVariables().put("osname", osName);
