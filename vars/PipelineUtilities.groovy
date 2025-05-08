4e98fbb9ab3fe70754410cfd65f93255a68ab2bc
Jason Katonica <katonica@us.ibm.com>
2025-05-08 10:44:39 -0400

    m

--- vars/PipelineUtilities.groovy
+++ vars/PipelineUtilities.groovy
@@ -6,64 +6,61 @@
  * this code, including the "Classpath" Exception described therein.
  */
 
-Class PipelineUtilities {
+/*
+ * Checks the checkboxes to figure out the platforms
+ * selected to build OpenJCEPlus on.
+ *
+ * @return      The platforms to build OpenJCEPlus on
+ */
+def getPlatforms() {
+    def platforms = []
 
-    /*
-     * Checks the checkboxes to figure out the platforms
-     * selected to build OpenJCEPlus on.
-     *
-     * @return      The platforms to build OpenJCEPlus on
-     */
-    def getPlatforms() {
-        def platforms = []
-    
-        if (PPC64_AIX == "true") {
-            platforms.add("ppc64_aix")
-        }
-    
-        if (X86_64_LINUX == "true") {
-            platforms.add("x86-64_linux")
-        }
-    
-        if (PPC64LE_LINUX == "true") {
-            platforms.add("ppc64le_linux")
-        }
-    
-        if (S390X_LINUX == "true") {
-            platforms.add("s390x_linux")
-        }
-    
-        if (X86_64_WINDOWS == "true") {
-            platforms.add("x86-64_windows")
-        }
-    
-        if (AARCH64_MAC == "true") {
-            platforms.add("aarch64_mac")
-        }
-    
-        if (X86_64_MAC == "true") {
-            platforms.add("x86-64_mac")
-        }
-    
-        if (AARCH64_LINUX == "true") {
-            platforms.add("aarch64_linux")
-        }
-    
-        return platforms
+    if (PPC64_AIX == "true") {
+        platforms.add("ppc64_aix")
     }
-    
-    def renameFilesInDirectory(String directory, String prefix) {
-        def sourceDirectory = new File(directory)
-        def files = sourceDirectory.listFiles().findAll { it.isFile() }
-        files.each { file ->
-            def originalName = file.getName()
-            def newName = "${prefix}${originalName}"
-            def newFile = new File(directory, newName)
-            if (file.renameTo(newFile)) {
-                echo "File renamed from ${originalName} to ${newName}"
-            } else {
-                echo "Failed to rename file from ${originalName} to ${newName}"
-            }
+
+    if (X86_64_LINUX == "true") {
+        platforms.add("x86-64_linux")
+    }
+
+    if (PPC64LE_LINUX == "true") {
+        platforms.add("ppc64le_linux")
+    }
+
+    if (S390X_LINUX == "true") {
+        platforms.add("s390x_linux")
+    }
+
+    if (X86_64_WINDOWS == "true") {
+        platforms.add("x86-64_windows")
+    }
+
+    if (AARCH64_MAC == "true") {
+        platforms.add("aarch64_mac")
+    }
+
+    if (X86_64_MAC == "true") {
+        platforms.add("x86-64_mac")
+    }
+
+    if (AARCH64_LINUX == "true") {
+        platforms.add("aarch64_linux")
+    }
+
+    return platforms
+}
+
+def renameFilesInDirectory(String directory, String prefix) {
+    def sourceDirectory = new File(directory)
+    def files = sourceDirectory.listFiles().findAll { it.isFile() }
+    files.each { file ->
+        def originalName = file.getName()
+        def newName = "${prefix}${originalName}"
+        def newFile = new File(directory, newName)
+        if (file.renameTo(newFile)) {
+            echo "File renamed from ${originalName} to ${newName}"
+        } else {
+            echo "Failed to rename file from ${originalName} to ${newName}"
         }
     }
 }
