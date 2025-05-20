0b63608666177d372a2901c01fee95ec86d0df78
Jason Katonica <katonica@us.ibm.com>
2025-05-20 15:17:10 -0400

    Working JMH example
    JMH maven and Java code update

---
+++ src/test/java/ibm/jceplus/jmh/RunAll.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright IBM Corp. 2023, 2025
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms provided by IBM in the LICENSE file that accompanied
+ * this code, including the "Classpath" Exception described therein.
+ */
+
+package ibm.jceplus.jmh;
+
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+
+public class RunAll extends OpenJCEPlusBase {
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = optionsBuild(
+            "Benchmark",
+            RunAll.class.getSimpleName());
+        new Runner(opt).run();
+    }
+}
