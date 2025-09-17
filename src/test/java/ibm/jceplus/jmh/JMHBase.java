a70c38c387ff1f0b49db0708e877dddab404fab1
Jason Katonica <katonica@us.ibm.com>
2025-09-17 15:18:30 -0400

    60 percent

--- src/test/java/ibm/jceplus/jmh/JMHBase.java
+++ src/test/java/ibm/jceplus/jmh/JMHBase.java
@@ -49,7 +49,7 @@ abstract public class JMHBase {
         optionsBuilder.addProfiler(CompilerProfiler.class);
         optionsBuilder.jvmArgsAppend("-Xms1G", "-Xmx1G", "--patch-module",
                 "openjceplus=" + projectHomeDir + "/target/classes",
-                "-Dmy.maxMemory=.80",
+                "-Dmy.maxMemory=.60",
                 "--add-exports=java.base/sun.security.util=ALL-UNNAMED",
                 "--add-exports=java.base/sun.security.pkcs=ALL-UNNAMED",
                 "--add-exports=java.base/sun.security.x509=ALL-UNNAMED",
