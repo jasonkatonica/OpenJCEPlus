4c356dbe9cb51012a1d5614129d8eed9927a3c71
Jason Katonica <katonica@us.ibm.com>
2026-04-17 12:30:45 -0400

    remove session import

--- src/test/java/ibm/jceplus/jmh/TLSHandshakeBenchmark.java
+++ src/test/java/ibm/jceplus/jmh/TLSHandshakeBenchmark.java
@@ -23,7 +23,6 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLParameters;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
-import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManagerFactory;
